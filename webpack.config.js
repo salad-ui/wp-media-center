ce4d46b9f0d5588b004d1816c45aea09ee59aefe
James Newell <jameslnewell@users.noreply.github.com>
2019-11-11 13:54:58 +0100

    Fix CI (#7)
    * yarn install salad-ui/components
    * alias all the @salad-ui/* components
    * use the wp-media-center version of react and styled-components
    * pin the version of typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-
-console.log({
-  '@salad-ui/typography': `${__dirname}/../components/packages/typography/src/index.tsx`,
-});
+const RegExpAliasResolverPlugin = require('./webpack-regexp-alias-resolver-plugin');
 
 module.exports = {
   entry: {
@@ -17,10 +14,16 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
     alias: {
-      '@salad-ui/typography': path.resolve(
-        `${__dirname}/../components/packages/typography/src`,
-      ),
+      react: require.resolve('react'),
+      'react-dom': require.resolve('react-dom'),
+      'styled-components': require.resolve('styled-components'),
     },
+    plugins: [
+      new RegExpAliasResolverPlugin(
+        /^@salad-ui\/(.+)/,
+        path.resolve(`${__dirname}/../components/packages/$1/src`),
+      ),
+    ],
   },
   module: {
     rules: [
